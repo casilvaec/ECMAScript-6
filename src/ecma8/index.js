c3562d5ba1167348b25deec94c5bcc0c91fe5bc3
Carlos Silva <casilva2@utpl.edu.ec>
2021-06-09 11:21:35 -0500

    Async/await

--- src/ecma8/index.js
+++ src/ecma8/index.js
@@ -31,6 +31,36 @@ console.log(string.padStart(8, 'hi '));
 console.log(string.padEnd(9, ' end'));
 
 
+//Async await
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    (true)
+      ? setTimeout (() => resolve ('Hello world'), 3000)
+      : reject (new Error ('Test Error'))
+  })
+};
+
+const helloAsync = async () => {
+  const hello = await helloWorld();
+  console.log(hello);
+};
+
+helloAsync ();
+
+//Otra manera
+const anotherFunction = async () => {
+  try {
+    const hello = await helloWorld();
+    console.log(hello);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+anotherFunction();
+
+
+
 
 
 
