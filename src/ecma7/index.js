d911f589f9445eb9ee9cfdfa68f0c5fdf16a8438
Carlos Silva <casilva2@utpl.edu.ec>
2021-06-09 08:58:16 -0500

    Ecma 7: includes y potencia **

---
+++ src/ecma7/index.js
@@ -0,0 +1,16 @@
+//includes para saber si un valor se encuentra dentro de un arreglo
+let numbers = [1, 2, 3, 15, 20];
+
+if (numbers.includes(15)){
+  console.log ('Si se encuentra el valor de 15');
+}else {
+  console.log('No se encuentra el valor de 15');
+}
+
+//cómo elevar un número a la potencia
+
+let base = 10;
+let exponent = 3;
+let result = base ** exponent;
+
+console.log(result);
